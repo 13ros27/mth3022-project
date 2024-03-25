d4d6b9ec517d65e0a1c1368a7f90cd4dd4607fa5
William Rose <william@theroses.me.uk>
2024-03-25 19:14:50 +0000

    Change from a hardcoded module path

--- python/__main__.py
+++ python/__main__.py
@@ -10,11 +10,11 @@ def read_syllabus_plan(file: str, *, start: str = 'SYLLABUS PLAN - summary of th
     text = '\n'.join([page.extract_text() for page in pages])
     return text[text.find(start) + len(start):text.find(end)]
 
-def read_all_files(folder: str, *, start: str = 'SYLLABUS PLAN - summary of the structure and academic content of the module', end: str = 'LEARNING AND TEACHING') -> dict[str, str]:
+def read_all_files(folder: str = '../modules', *, start: str = 'SYLLABUS PLAN - summary of the structure and academic content of the module', end: str = 'LEARNING AND TEACHING') -> dict[str, str]:
     return {f[f.find('MTH'):f.find('MTH')+7]: read_syllabus_plan(folder + '/' + f, start=start, end=end) for f in listdir(folder)}
 
 if __name__ == '__main__':
-    words_map = {k: words.split(v) for (k, v) in read_all_files('C:/Users/willi/Downloads/ModuleInfo').items()}
+    words_map = {k: words.split(v) for (k, v) in read_all_files().items()}
     depths = {}
     for (k1, v1) in words_map.items():
         for (k2, v2) in words_map.items():
